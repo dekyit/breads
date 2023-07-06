7c49459dac5d24696247ebdb7993fd99971d7d9d
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-07-06 19:25:05 -0400

    sending index and show data

---
+++ server.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
+
+const breads = express.Router()
+
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
