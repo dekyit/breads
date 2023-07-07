5043fad563fdc08e0ae6b3c69979f30a08e8f829
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-07-06 21:37:19 -0400

    changed port

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
-
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
-
 const breads = express.Router()
 
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
